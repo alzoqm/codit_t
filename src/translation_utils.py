de948de6d8aba47d5ccd95d04e641458a3642da1
alzoqm <70330480+alzoqm@users.noreply.github.com>
2025-06-03 23:28:56 +0900

    Update translation_utils.py

--- src/translation_utils.py
+++ src/translation_utils.py
@@ -113,7 +113,7 @@ def get_hf_model_and_tokenizer(
         if model_subfolder is None:
             model = AutoModelForSeq2SeqLM.from_pretrained(pretrained_model_name_or_path=repo_id, **model_load_kwargs)
         else:
-            model = AutoModelForSeq2SeqLM.from_pretrained(pretrained_model_name_or_path=repo_id, )
+            model = AutoModelForSeq2SeqLM.from_pretrained(pretrained_model_name_or_path=repo_id, device_map=device, torch_dtype=torch.float16,)
         print("Standard model loaded.")
     
     model.eval()
